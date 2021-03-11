6278d5a9c4a507c26ba5d2d5a6b81ea6661390f6
I-RInka <I_Rinka@outlook.com>
2021-03-11 16:05:19 +0800

    assignment 1 done

--- assignment1/check_proc_alive.sh
+++ assignment1/check_proc_alive.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+#检查并格式化进程
 check_process()
 {
-    ps -aux | grep $1 | grep -v grep
+    ans=$(ps -aux | grep $1 | grep -v grep)
+
+    if (($? != 0))
+    then
+        return -1
+    else
+        echo "$ans" | awk '{
+        print $2
+        }'
+    fi
 }
 
 file=`cat ./check_procs.conf`
 
+#检查文件打开情况
 if (( $? != 0 ))
 then
     echo "config file open error" >/dev/stderr
@@ -14,19 +25,22 @@ fi
 
 for line in $file
 do
+    #判断是否有注释
     if [[ ${line:0:1} == "#" ]]
     then
         continue
     fi
 
+    #得到awk
     ans=$(check_process $line)
 
+    #判断是否查到进程
     if (( $? == 0 ))
     then
-    #进程还活着，对ans进行字符串处理
-        echo $line $ans
+        echo $line: $ans
+
     else
-        echo $line DOWN
+        echo $line: DOWN
     fi
 
 done
